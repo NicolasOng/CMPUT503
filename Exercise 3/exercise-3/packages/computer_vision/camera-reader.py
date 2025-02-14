f065ec1064f512cb3a2d8d129f62bee0209f2553
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-02-14 16:55:02 -0700

    Added Martins camera undistortion code

---
+++ Exercise 3/exercise-3/packages/computer_vision/camera-reader.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from sensor_msgs.msg import CompressedImage
+
+import cv2
+from cv_bridge import CvBridge
+
+class CameraReaderNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(CameraReaderNode, self).__init__(node_name=node_name, node_type=NodeType.VISUALIZATION)
+        # static parameters
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
+        # bridge between OpenCV and ROS
+        self._bridge = CvBridge()
+        # create window
+        self._window = "camera-reader"
+        cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
+        # construct subscriber
+        self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
+
+    def callback(self, msg):
+        # ******HAVE TO PUBLISH THE IMAGE GENERATED BELOW TO A TOPIC, VIEW IT WITH rqt_image_view
+        # convert JPEG bytes to CV image
+        image = self._bridge.compressed_imgmsg_to_cv2(msg)
+
+        # camera matrix and distortion coefficients from intrinsic.yaml file
+        cam_matrix = np.array([[319.2461317458548, 0.0, 307.91668484581703], [0.0, 317.75077109798957, 255.6638447529814], [0.0, 0.0, 1.0]])
+        dist_coeff = np.array([-0.25706255601943445, 0.045805679651939275, -0.0003584336283982042, -0.0005756902051068707, 0.0])
+
+        # image dimensions
+        h,  w = image.shape[:2]
+        # optimal camera matrix lets us see entire camera image (image edges cropped without), but some distortion visible
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cam_matrix, dist_coeff, (w,h), 0, (w,h))
+
+        # undistorted image using calibration parameters
+        new_image = cv2.undistort(image, cam_matrix, dist_coeff, None)
+        
+        # display frame
+        cv2.imshow(self._window, new_image)
+        cv2.waitKey(1)
+
+if __name__ == '__main__':
+    # create the node
+    node = CameraReaderNode(node_name='camera_reader_node')
+    
+    # keep spinning
+    rospy.spin()
