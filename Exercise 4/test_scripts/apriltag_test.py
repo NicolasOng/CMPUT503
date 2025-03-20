5a0af9ac73193742a1466d1841364cde551b3143
Martin McLaren <mamclare@ualberta.ca>
2025-03-20 15:11:27 -0600

    Fixing sorting tags by area

--- Exercise 4/test_scripts/apriltag_test.py
+++ Exercise 4/test_scripts/apriltag_test.py
@@ -5,10 +5,28 @@ if __name__ == '__main__':
     
     img = cv.imread('atag.jpeg')
     img_grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    image_grey = cv2.GaussianBlur(image_grey, (5,5), 0)
 
     detector = apriltag.Detector()
     results = detector.detect(img_grey)
 
+    tags_list = []
+
+    largest_tag_index = 0
+    largest_tag_area = 0
+    
+    '''
+    # If multiple tags detected, find most prominent tag (largest by area)
+    if len(results) > 1:
+        for idx, r in enumerate(results):
+            tl = r.corners[0].astype(int)
+            br = r.corners[2].astype(int)
+            area = (tl[1] - br[1]) * (br[0] - tl[0])
+            if area > largest_tag_area:
+                largest_tag_index = idx
+                largest_tag_area = area
+    '''
+    
     print("Number of detected tags: ", len(results))
     count = 0 
     for r in results:
